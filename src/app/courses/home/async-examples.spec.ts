3319b615bfa53660e2a0599f419ce6ec913ccecc
dawmckin <dawmckin20@gmail.com>
2023-02-26 16:19:29 -0500

    async testing

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,151 +1,109 @@
-import {fakeAsync, flush, flushMicrotasks, tick} from '@angular/core/testing';
-import {of} from 'rxjs';
-import {delay} from 'rxjs/operators';
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
+
+describe("Async Testing Examples", () => {
+  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
+    let test = false;
+
+    setTimeout(() => {
+
+      console.log('running assertions');
+
+      test = true;
+
+      expect(test).toBeTruthy();
+
+      done();
+    }, 1000);
+  });
+
+  it("Asynchronous test example - fakeAsync", fakeAsync(() => {
+
+    let test = false;
+
+    setTimeout(() => {
+
+      console.log('running assertions');
+
+      test = true;
+
+    }, 1000);
+
+    flush();
+
+    expect(test).toBeTruthy();
+
+  }));
+
+  it('Asynchronous test example - plain Promise', fakeAsync(() => {
+
+    let test = false;
+
+    console.log('Creating Promise');
+
+    Promise.resolve().then(() => {
+      console.log('Promise first then() evaluted successfully');
+      test = true;
+      return Promise.resolve();
+    }).then(() => {
+      console.log('Promise second then() evaluted successfully');
+    });
+
+    flushMicrotasks();
+
+    console.log('running assertions');
+
+    expect(test).toBeTruthy();
 
 
-describe('Async Testing Examples', () => {
+  }));
 
-    it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
+  it('Asynchronous test example - Promise + setTimeout()', fakeAsync(() => {
 
-        let test = false;
+    let counter = 0;
 
-        setTimeout(() => {
+    Promise.resolve().then(() => {
 
-            console.log('running assertions');
+      counter += 10;
 
-            test = true;
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
 
-            expect(test).toBeTruthy();
+    expect(counter).toBe(0);
 
-            done();
+    flushMicrotasks();
 
-        }, 1000);
+    expect(counter).toBe(10);
+
+    flush();
+
+    expect(counter).toBe(11);
+
+  }));
+
+  it('Asynchronous test example - Observables', fakeAsync(() => {
+
+    let test = false;
+
+    console.log('Create Observable');
+
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+
+      test = true;
 
     });
 
+    tick(1000);
 
-    it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+    console.log('running assertions');
 
-        let test = false;
+    expect(test).toBe(true);
 
-        setTimeout(() => {
-        });
-
-        setTimeout(() => {
-
-            console.log('running assertions setTimeout()');
-
-            test = true;
-
-        }, 1000);
-
-        flush();
-
-        expect(test).toBeTruthy();
-
-    }));
-
-
-    it('Asynchronous test example - plain Promise', fakeAsync(() => {
-
-        let test = false;
-
-        console.log('Creating promise');
-
-        Promise.resolve().then(() => {
-
-            console.log('Promise first then() evaluated successfully');
-
-            return Promise.resolve();
-        })
-        .then(() => {
-
-            console.log('Promise second then() evaluated successfully');
-
-            test = true;
-
-        });
-
-        flushMicrotasks();
-
-        console.log('Running test assertions');
-
-        expect(test).toBeTruthy();
-
-    }));
-
-
-    it('Asynchronous test example - Promises + setTimeout()', fakeAsync(() => {
-
-        let counter = 0;
-
-        Promise.resolve()
-            .then(() => {
-
-               counter+=10;
-
-               setTimeout(() => {
-
-                   counter += 1;
-
-               }, 1000);
-
-            });
-
-        expect(counter).toBe(0);
-
-        flushMicrotasks();
-
-        expect(counter).toBe(10);
-
-        tick(500);
-
-        expect(counter).toBe(10);
-
-        tick(500);
-
-        expect(counter).toBe(11);
-
-    }));
-
-    it('Asynchronous test example - Observables', fakeAsync(() => {
-
-        let test = false;
-
-        console.log('Creating Observable');
-
-        const test$ = of(test).pipe(delay(1000));
-
-        test$.subscribe(() => {
-
-            test = true;
-
-        });
-
-        tick(1000);
-
-        console.log('Running test assertions');
-
-        expect(test).toBe(true);
-
-
-    }));
-
+  }));
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,39 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
-
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
-    let calculator: CalculatorService,
-        loggerSpy: any;
+  let calculator: CalculatorService,
+    loggerSpy: any;
 
-    beforeEach(()=> {
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
-        console.log("Calling beforeEach");
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    })
 
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    calculator = TestBed.get(CalculatorService);
 
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
-            ]
-        });
+  });
 
-        calculator = TestBed.get(CalculatorService);
+  it('should add two numbers', () => {
+    const result = calculator.add(2,2);
 
-    });
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 
-    it('should add two numbers', () => {
+  it('should subtract two numbers', () => {
+    const result = calculator.subtract(2,2);
 
-        console.log("add test");
+    expect(result).toBe(0, "unexpected subtraction result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
-        const result = calculator.add(2, 2);
-
-        expect(result).toBe(4);
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
-
-    it('should subtract two numbers', () => {
-
-        console.log("subtract test");
-
-        const result = calculator.subtract(2, 2);
-
-        expect(result).toBe(0, "unexpected subtraction result");
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
+  });
 
 });
